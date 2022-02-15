24aa5ac12162db9684034aff131c29e209877019
molevmichail <73595754+molevmichail@users.noreply.github.com>
2022-02-15 22:19:18 +0300

    Update arithmetic.h

--- include/arithmetic.h
+++ include/arithmetic.h
@@ -1,58 +1,27 @@
-// объявление функций и классов для вычисления арифметических выражений
+// Объявление функций и классов для вычисления арифметических выражений
 #ifndef __ARITHMETIC_H__
 #define __ARITHMETIC_H__
 #include <iostream>
 #include <string>
 using namespace std;
-
-class Tlexeme
-{
-protected:
-	int Size;
-	string* mLexeme; //массив строк разбитых на лексемы
-	int* priority;   //приоритет лексем
-	int Flag = -1;   //флаг обработки
-public:
-	//конструкторы и деструкторы
-	Tlexeme(string a = "0");
-	~Tlexeme();
-	//операции
-	void Fill(string a); //загрузить новую строку
-	int Check_correct(); //проверка на корректность введения записи
-	string Pol(); //вернуть польскую строку
-	double Calculation(int x = 0); //вычисление
-	//перегрузка операций ввода/вывода
-	friend istream& operator>>(istream& in, Tlexeme& l)
-	{
-		in >> l.mLexeme[0];
-		string a = l.mLexeme[0];
-		l.Size = (l.mLexeme[0]).length();
-		l.mLexeme = new string[l.Size];
-		l.priority = new int[l.Size];
-		l.mLexeme[0] = a;
-		return in;
-	}
-	friend ostream& operator<<(ostream& out, const Tlexeme& l)
-	{
-		for (int i = 0; i < l.Size; i++)
-		{
-			out << l.mLexeme[i] << ' ';
-		}
-		cout << endl;
-		/*for (int i = 0; i < l.Size; i++)
-		{ out << l.priority[i] << ' ';}
-		cout << endl;*/
-		return out;
-	}
-private:
-	//закрытые функции
-	void Divide(); //разбить на лексемы
-	void Pol_1(); //конвертировать в польскую запись
-	int Check_brack();//проверка скобок
-	int Check_sign(); //проверка знаков операций
-	int Check_point(); //проверка точек в дробных числах
-	int Check_variable();//проверка переменных
-	int Check_function();//проверка функции
+class Tlexeme 
+{protected:
+  int Size;
+  string* mLexeme; // массив строк разбитых на лексемы
+  int* priority;   // приоритет лексем
+ public:
+  //Конструкторы и деструкторы
+  Tlexeme(string a);
+  ~Tlexeme();
+  //Операции
+  int Check_correct(); //Проверка на корректность введения записи
+  string Pol(); // Вернуть польскую строку 
+  double Calculation(int x=0); // Вычисление
+  
+ private:
+  // Закрытые функции
+  int Check_brack();//Проверка скобок
+  int Check_sign(); //Проверка знаков операций
 };
 
 #endif
