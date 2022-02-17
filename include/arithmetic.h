9f395cb07ae5798069f446d8e7fc10fc12f1796a
molevmichail <73595754+molevmichail@users.noreply.github.com>
2022-02-17 12:08:14 +0300

    Update arithmetic.h

--- include/arithmetic.h
+++ include/arithmetic.h
@@ -5,23 +5,24 @@
 #include <string>
 using namespace std;
 class Tlexeme 
-{protected:
-  int Size;
-  string* mLexeme; // массив строк разбитых на лексемы
-  int* priority;   // приоритет лексем
- public:
-  //Конструкторы и деструкторы
-  Tlexeme(string a);
-  ~Tlexeme();
-  //Операции
-  int Check_correct(); //Проверка на корректность введения записи
-  string Pol(); // Вернуть польскую строку 
-  double Calculation(int x=0); // Вычисление
+{
+protected:
+     int Size;
+     string* mLexeme; // массив строк разбитых на лексемы
+     int* priority;   // приоритет лексем
+public:
+     //Конструкторы и деструкторы
+     Tlexeme(string a);
+     ~Tlexeme();
+     //Операции
+     int Check_correct(); //Проверка на корректность введения записи
+     string Pol(); // Вернуть польскую строку 
+     double Calculation(int x=0); // Вычисление
   
- private:
-  // Закрытые функции
-  int Check_brack();//Проверка скобок
-  int Check_sign(); //Проверка знаков операций
+private:
+     // Закрытые функции
+     int Check_brack();//Проверка скобок
+     int Check_sign(); //Проверка знаков операций
 };
 
 #endif
